6ec631733a664265daa52fa21cd5fcf3ecb2062b
Nitram <nitram@b28f455a-2cb1-40d1-ba0a-a80b42ff2d3d.home>
2026-09-22 13:40:58 +0200

    chore: wire up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,53 @@
-import { Button } from "@/components/ui/button"
+import { Routes, Route } from "react-router"
+
+import { AppShaderBackground } from "./components/layout/ShaderBackground"
+import Navbar from "./components/layout/Navbar"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/auth/LoginPage"
+import SignUpPage from "./pages/auth/SignUpPage"
+import NotFoundPage from "./pages/NotFoundPage"
+import AccountPage from "./pages/AccountPage"
+import FeedPage from "./pages/FeedPage"
+import EmailVerificationPage from "./pages/email/EmailVerificationPage"
+import VerifyEmailPage from "./pages/email/VerifyEmailPage"
+import { ProtectedRoute } from "./components/ProtectedRoute"
+import ForgotPasswordPage from "./pages/password/ForgotPasswordPage"
+import ResetPasswordPage from "./pages/password/ResetPasswordPage"
 
 export function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
+    <>
+      <AppShaderBackground />
+
+      <header className="mx-auto max-w-7xl">
+        <Navbar />
+      </header>
+
+      <main className="mx-auto max-w-7xl">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/feed" element={<FeedPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+
+          <Route
+            path="/email-verification"
+            element={<EmailVerificationPage />}
+          />
+          <Route path="/email/verify/:id/:hash" element={<VerifyEmailPage />} />
+
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/account" element={<AccountPage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+      
+      <footer className="mx-auto max-w-7xl"></footer>
+    </>
   )
 }
 
